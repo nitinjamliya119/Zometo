773e9580622dc22dda27ab9fabedf77db1af8c05
nitinjamliya119 <nitinjamliya119@gmail.com>
2025-06-13 14:21:37 +0530

    i

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Header/Navbar'
+import DiningOut from '../About/Dining-out/DiningOut'
+
+
+function Home() {
+  return (<>
+       <Navbar/>
+       <DiningOut/>
+       
+       
+
+        
+       
+
+
+    </>)
+}
+
+export default Home
